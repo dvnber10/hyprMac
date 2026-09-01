503a6d469683f08ee391b7f975a6850967bde01e
dvnber10 <dvnber2001@gmail.com>
2026-09-01 13:05:06 -0500

    fix: dock_launcher.sh - usar $HOME en vez de ~

--- config/nwg-dock-hyprland/dock_launcher.sh
+++ config/nwg-dock-hyprland/dock_launcher.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 # Launcher del dock - abre rofi al hacer click en el icono
-rofi -show drun -theme ~/.config/rofi/launchpad.rasi &
+rofi -show drun -theme "$HOME/.config/rofi/launchpad.rasi" &
